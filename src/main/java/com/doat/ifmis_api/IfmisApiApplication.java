9a5c39cd124ac3b81442bd39f1d7b4a92f915d7d
MadhurjyaMai <mmsutradhar@gmail.com>
2024-12-05 15:18:36 +0530

    Initial Commit

---
+++ src/main/java/com/doat/ifmis_api/IfmisApiApplication.java
@@ -0,0 +1,13 @@
+package com.doat.ifmis_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfmisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfmisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doat/ifmis_api/IfmisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doat.ifmis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfmisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
