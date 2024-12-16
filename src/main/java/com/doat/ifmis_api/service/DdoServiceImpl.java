e63a4c8c5b98591898fcc29bf2f4f57a96e74daa
MadhurjyaMai <mmsutradhar@gmail.com>
2024-12-16 16:12:44 +0530

    Implemented Query Service

--- src/main/java/com/doat/ifmis_api/service/DdoServiceImpl.java
+++ src/main/java/com/doat/ifmis_api/service/DdoServiceImpl.java
@@ -16,50 +16,30 @@ public class DdoServiceImpl implements DdoService {
 
     private EntityManager em;
 
+    private QueryService queryService;
+
     private static final Logger logger = LogManager.getLogger(DdoServiceImpl.class);
 
     @Override
     public DdoModel getDdoDetails(String ddoCode) {
 
-        String queryString = "SELECT \n" +
-                "    ds.district_Name,\n" +
-                "    t.hierarchy_Name ,\n" +
-                "    t.hierarchy_Code ,\n" +
-                "    hs.hierarchy_Code ddo_code,\n" +
-                "    o.office_Name,\n" +
-                "    us.title_Name,\n" +
-                "    us.user_Name,\n" +
-                "    us.last_Name,\n" +
-                "    us.email,\n" +
-                "    us.mobile\n" +
-                "FROM\n" +
-                "    pfmaster.hierarchy_setup AS hs\n" +
-                "    \t  JOIN pfmaster.hierarchy_setup o\n" +
-                "    ON hs.parent_hierarchy = o.hierarchy_Id\n" +
-                "        JOIN\n" +
-                "    probityfinancials.ddo_setup AS s ON s.ddo_id = hs.hierarchy_Id\n" +
-                "        JOIN\n" +
-                "    probityfinancials.district_setup AS ds ON ds.district_Id = s.district_id\n" +
-                "        JOIN\n" +
-                "    pfmaster.hierarchy_setup AS t ON t.hierarchy_Id = s.treasury_id\n" +
-                "        JOIN\n" +
-                "    pfmaster.seat_user_alloted AS sua ON sua.seat_id = hs.hierarchy_Id\n" +
-                "        JOIN\n" +
-                "    pfmaster.user_setup AS us ON us.user_Id = sua.user_Id\n" +
-                "WHERE\n" +
-                "    hs.category = 'S'\n" +
-                "    AND sua.active_Status = 'Y'\n" +
-                "    AND hs.hierarchy_Code = '" + ddoCode + "'";
+        try {
+            String queryString = queryService.getDdoDetails(ddoCode);
+            Query query = em.createNativeQuery(queryString);
 
-        Query query = em.createNativeQuery(queryString);
+            List<Object[]> ddoObjectList = query.getResultList();
 
-        List<Object[]> ddoObjectList = query.getResultList();
-
-        if (!ddoObjectList.isEmpty()) {
-            DdoModel ddoModel = getDdoModel(ddoObjectList);
-            logger.debug("DB Response == {} ", ddoModel);
-            return ddoModel;
+            if (!ddoObjectList.isEmpty()) {
+                DdoModel ddoModel = getDdoModel(ddoObjectList);
+                logger.debug("DB Response == {} ", ddoModel);
+                return ddoModel;
+            }
         }
+        catch (Exception e)
+        {
+            logger.warn("Exception Occured with {}", e.getMessage());
+        }
+
 
         return null;
     }
